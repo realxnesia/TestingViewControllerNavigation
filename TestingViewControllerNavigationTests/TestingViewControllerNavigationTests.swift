13e6e507c3deff65368b295923f587b7921d2aa9
dhika <dhika.aditya@kpvendor.id>
2023-01-26 08:26:17 +0700

    fix bug storyboard identifier and class, Test push UIVIewController into UINavigationController. Wait for expectation

--- TestingViewControllerNavigationTests/TestingViewControllerNavigationTests.swift
+++ TestingViewControllerNavigationTests/TestingViewControllerNavigationTests.swift
@@ -11,6 +11,7 @@ import XCTest
 final class TestingViewControllerNavigationTests: XCTestCase {
 
     var sut: ViewController!
+    var navigationController: UINavigationController!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,26 +23,28 @@ final class TestingViewControllerNavigationTests: XCTestCase {
         
         // Step 3. Make the viewDidLoad() execute
         sut.loadViewIfNeeded()
+        navigationController = UINavigationController(rootViewController: sut) //root-nya SUT
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sut = nil
+        navigationController = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_NextViewButton_WhenTapped_SecondViewControllerIsPushed() {
+        let myPredicate = NSPredicate { input, _ in
+            return (input as? UINavigationController)?.topViewController is SecondViewController
+        }
+        expectation(for: myPredicate, evaluatedWith: navigationController)
+        
+        sut.nextViewButton.sendActions(for: .touchUpInside)
+        
+        waitForExpectations(timeout: 1.5)
+        
+        guard let _ = navigationController.topViewController as? SecondViewController else {
+            XCTFail()
+            return
         }
     }
-
 }
